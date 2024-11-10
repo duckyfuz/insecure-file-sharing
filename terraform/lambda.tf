34608a4e14cb0e2e32edce04a2049ece624a7cfd
Kenneth Gao <108561447+duckyfuz@users.noreply.github.com>
2024-11-10 23:52:45 +0800

    bump python version

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -9,7 +9,7 @@ resource "aws_lambda_function" "upload_function" {
   function_name    = "upload_function"
   role             = aws_iam_role.lambda_role.arn
   handler          = "upload.lambda_handler"
-  runtime          = "python3.8"
+  runtime          = "python3.11"
   source_code_hash = data.archive_file.lambda.output_base64sha256
 }
 
