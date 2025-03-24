2a3726c3d1baf1445b3023bcd12aa5517a7b032a
duckyfuz <108561447+duckyfuz@users.noreply.github.com>
2025-03-24 17:23:48 +0800

    remove acl

--- upload.py
+++ upload.py
@@ -15,10 +15,10 @@ def lambda_handler(event, context):
         bucket_name = "ifs-storage-bucket"
         file_name = json.loads(event["body"])["file_name"]
 
-        fields = {"acl": "public-read"}
+        # fields = {"acl": "public-read"}
  
         conditions = [
-            {"acl": "public-read"},
+            # {"acl": "public-read"},
             ["content-length-range", 1, 10485760] # allow 10MiB size
         ]
         # TODO: add filename for downloading
@@ -26,7 +26,7 @@ def lambda_handler(event, context):
         presigned_url = s3_client.generate_presigned_post(
             Bucket=bucket_name,
             Key=file_name,
-            Fields=fields,
+            Fields={},
             Conditions=conditions
         )
 
