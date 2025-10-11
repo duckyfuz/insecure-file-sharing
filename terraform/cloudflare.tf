e5e8a77e17f88d5256a95bbe1cc1cf4d73719cd8
duckyfuz <108561447+duckyfuz@users.noreply.github.com>
2025-10-11 14:05:15 +0800

    remove cf acm cert comment

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -10,7 +10,7 @@ locals {
 
 resource "cloudflare_record" "acm_certificate_cname" {
   zone_id = local.cloudflare_zone_id
-  comment = "${local.comment_prefix} - for AWS ACM certificate"
+  # comment = "${local.comment_prefix} - for AWS ACM certificate"
   name    = local.acm_certificate_record_name
   type    = local.acm_certificate_record_type
   content = local.acm_certificate_record_value
