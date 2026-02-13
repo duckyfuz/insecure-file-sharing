80796b330596ccbf8dd5e868d5fc6873177a6efc
duckyfuz <108561447+duckyfuz@users.noreply.github.com>
2026-02-13 17:29:45 +0800

    fix: add iam for lambda to cw

--- infra/iam.tf
+++ infra/iam.tf
@@ -44,3 +44,29 @@ resource "aws_iam_role_policy_attachment" "combined_policy_attachment" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.allow_s3_access.arn
 }
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "${var.project_name}_lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Resource" : "arn:aws:logs:*:*:*",
+        "Effect" : "Allow"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
